994e6f54ce1d3ae82e8143e8a29a6cd46f95afbe
ItsPoptartDesu <davidmejia4215@gmail.com>
2023-09-18 22:36:29 -0700

    updated app name

--- NADCL/resources/views/landing.blade.php
+++ NADCL/resources/views/landing.blade.php
@@ -368,7 +368,7 @@ $video3 = str_replace('watch?v', 'embed/', $videoURL3);
                 </div>
             </div>
         </div>
-    </div>
+    {{-- </div> --}}
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
